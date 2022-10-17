52429581f232dcedc4d48b3c4ed2325cb5ecc737
VanTruongUIT <tvtruong.uit@gmail.com>
2022-10-17 07:13:41 +0700

    using promise instead of callback

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -31,58 +31,58 @@ const renderCountry = function(data) {
     countriesContainer.style.opacity = 1;    
 }
 
-const getCountryAndNeighbour = function(countryName) {
-    // Create a new request
-    // AJAX call country 1
-    const request = new XMLHttpRequest();
-    // open a new get request with endpoint
-    request.open(
-        'GET',
-        `${rootEndpoint}/name/${countryName}`
-    );
-    // we can not assign request.send() to a variable. Because it's a asynchronous code, it runs in the background, it was taked time to send and response data.
-    request.send();
-    // so we need register a callback function to a load event to process data immediately after we get the data from server
-    request.addEventListener('load', () => {
-        // we will use the this -> this of request context
-        const [data] = JSON.parse(request.responseText);
-        console.log('data: ', data);
+// const getCountryAndNeighbour = function(countryName) {
+//     // Create a new request
+//     // AJAX call country 1
+//     const request = new XMLHttpRequest();
+//     // open a new get request with endpoint
+//     request.open(
+//         'GET',
+//         `${rootEndpoint}/name/${countryName}`
+//     );
+//     // we can not assign request.send() to a variable. Because it's a asynchronous code, it runs in the background, it was taked time to send and response data.
+//     request.send();
+//     // so we need register a callback function to a load event to process data immediately after we get the data from server
+//     request.addEventListener('load', () => {
+//         // we will use the this -> this of request context
+//         const [data] = JSON.parse(request.responseText);
+//         console.log('data: ', data);
 
-        // Render country 1
-        renderCountry(data);
+//         // Render country 1
+//         renderCountry(data);
 
 
-        // Get neighbour country (2)
-        const neighbour = data?.borders?.[0];
-        if (!neighbour) {
-            return;
-        }
-        // AJAX call country 2
-        console.log('neighbour: ', neighbour);
-        const request2 = new XMLHttpRequest();
-        // open a new get request with endpoint
-        request2.open(
-            'GET',
-            `${rootEndpoint}/alpha/${neighbour}`
-        );
-        // we can not assign request2.send() to a variable. Because it's a asynchronous code, it runs in the background, it was taked time to send and response data.
-        request2.send();
+//         // Get neighbour country (2)
+//         const neighbour = data?.borders?.[0];
+//         if (!neighbour) {
+//             return;
+//         }
+//         // AJAX call country 2
+//         console.log('neighbour: ', neighbour);
+//         const request2 = new XMLHttpRequest();
+//         // open a new get request with endpoint
+//         request2.open(
+//             'GET',
+//             `${rootEndpoint}/alpha/${neighbour}`
+//         );
+//         // we can not assign request2.send() to a variable. Because it's a asynchronous code, it runs in the background, it was taked time to send and response data.
+//         request2.send();
 
-        request2.addEventListener('load', () => {
-            const neighbourData = JSON.parse(request2.responseText);
-            console.log('Request two times for - neighbourData: ', neighbourData);
+//         request2.addEventListener('load', () => {
+//             const neighbourData = JSON.parse(request2.responseText);
+//             console.log('Request two times for - neighbourData: ', neighbourData);
 
-            renderCountry(neighbourData);
-        });
+//             renderCountry(neighbourData);
+//         });
 
-    });
-};
+//     });
+// };
 
-getCountryAndNeighbour('portugal');
+// getCountryAndNeighbour('portugal');
 // getCountryAndNeighbour('usa');
 // getCountryAndNeighbour('vietnam');
 
-const timeToLive = 0;
+// const timeToLive = 0;
 
 // ################################# CALLBACK HELL #################################
 /* Asynchronous code is not block the current execution engineer
@@ -92,16 +92,31 @@ const timeToLive = 0;
     for more information, please visit at: https://anhtruong.it.com/
 */
 // CALLBACK HELL
-setTimeout(() => {
-    console.log('Log setTimeOut at: ', 1);
-    setTimeout(() => {
-        console.log('Log setTimeOut at: ', 2);
-        setTimeout(() => {
-            console.log('Log setTimeOut at: ', 3);
-            setTimeout(() => {
-                console.log('Log setTimeOut at: ', 4);
+// setTimeout(() => {
+//     console.log('Log setTimeOut at: ', 1);
+//     setTimeout(() => {
+//         console.log('Log setTimeOut at: ', 2);
+//         setTimeout(() => {
+//             console.log('Log setTimeOut at: ', 3);
+//             setTimeout(() => {
+//                 console.log('Log setTimeOut at: ', 4);
                 
-            }, 1000);            
-        }, 1000);        
-    }, 1000);    
-}, 1000);
+//             }, 1000);            
+//         }, 1000);        
+//     }, 1000);    
+// }, 1000);
+
+
+/* 
+################################## PROMISE ################################## 
+*/
+const getCountryData = (countryName) => {
+    const request = fetch(
+        `${rootEndpoint}/name/${countryName}`
+    );
+    request
+        .then(response => response.json())
+        .then(data => renderCountry(data[0]))    
+}
+
+getCountryData('portugal');
