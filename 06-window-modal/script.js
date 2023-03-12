c39dcc7633cb300d2ad895858e5fb21b6622bb8c
VanTruongUIT <tvtruong.uit@gmail.com>
2023-03-12 22:26:21 +0700

    handle press esc key

--- 06-window-modal/script.js
+++ 06-window-modal/script.js
@@ -23,3 +23,16 @@ for (let index = 0; index < btnsOpenModal.length; index++) {
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+/* 
+Handle key press event
+Key event for all document, not for spedific any element.
+*/
+// when user press keydown to keyboard -> js create a new event to track the action of the user do it. If you want to access to this variable -> pass the argument to the function handle of the addEventListener
+document.addEventListener("keydown", function (event) {
+  console.log(event);
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    // check if modal contain hidden class, if don't have -> add hidden class
+    closeModal();
+  }
+});
