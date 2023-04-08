1b8ca0b0a311b9ca3d67fc238a13df7e99eec6be
VanTruongUIT <tvtruong.uit@gmail.com>
2023-04-08 09:40:16 +0700

    complete refactoring the architecture of the project

--- 15.mapty-app/script.js
+++ 15.mapty-app/script.js
@@ -17,77 +17,109 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-let map, mapEvent;
+class App {
+  // to keep the properties private
+  #map;
+  #mapEvent;
 
-// before handle function, check it exists
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      //   console.log("position: ", position);
-      // get the longitude and latitude value
-      const { latitude, longitude } = position.coords;
-      console.log(
-        `${MAP_BASE_URL}@${latitude},${longitude},${DEFAULT_MAP_ZOOM}`
+  // the constructor function will be called asap when the new object is created
+  constructor() {
+    // load the map based on the current position when the new object is created
+    this._getPosition();
+
+    // as you can see, there are 2 varialbe: map and mapEvent does not exists
+    // so we need create a global variable to store 2 variables
+    // the submit event with form element will trigger when any input element is enter key is press
+    form.addEventListener("submit", this._newWorkout.bind(this));
+
+    inputType.addEventListener("change", this._toggleElevationField);
+  }
+
+  _getPosition() {
+    console.log("this in _getPosotion", this);
+    // before handle function, check it exists
+    if (navigator.geolocation) {
+      // this._loadMap is call as a callback function in the getCurrentPosition in leaflef. indeed, this function is called as regular function, not is the method called.
+      // this._loadMap is the callback function, we not call it by ourself - App's instance
+      // if you want to bind the this keyword into the _loadMap function, we need to use the bind function
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Could not get your position");
+        }
       );
-
-      const coords = [latitude, longitude];
-
-      //   assign map variable
-      map = L.map("map").setView(coords, 13);
-
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      map?.on("click", function (mapE) {
-        //  assign the mapEvent equal to mapE
-        mapEvent = mapE;
-
-        form.classList.remove("hidden");
-        // when the use click on the map -> show the form and focus mouse on the distance input
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert("Could not get your position");
     }
-  );
+  }
+
+  _loadMap(position) {
+    //   console.log("position: ", position);
+    // get the longitude and latitude value
+    const { latitude, longitude } = position.coords;
+    console.log(`${MAP_BASE_URL}@${latitude},${longitude},${DEFAULT_MAP_ZOOM}`);
+
+    const coords = [latitude, longitude];
+
+    console.log("this at _loadMap", this);
+    //   assign map variable
+    this.#map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map?.on("click", this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    //  assign the mapEvent equal to mapE
+    this.#mapEvent = mapE;
+
+    form.classList.remove("hidden");
+    // when the use click on the map -> show the form and focus mouse on the distance input
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  _newWorkout(event) {
+    event.preventDefault();
+
+    //   clear input fields before input them
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        "";
+
+    console.log("this at _newWorkout", this.#mapEvent);
+
+    // display the marker
+    const { lat: currentLatitude, lng: currentLogitude } =
+      this.#mapEvent.latlng;
+    // add the current mouse click address to map
+    L.marker([currentLatitude, currentLogitude])
+      .addTo(this.#map)
+      //   binds a popup to the layer with the passed content
+      .bindPopup(
+        // a new popup instance
+        L.popup({
+          maxWidth: 250,
+          maxHeight: 50,
+          //   set to false if you want to do not close the popup when another popup is opened
+          autoClose: false,
+          //    set to false if you want to do not close the popup when use clicks on the map
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("Workout")
+      .openPopup();
+    form.classList.add("hidden");
+  }
 }
 
-// as you can see, there are 2 varialbe: map and mapEvent does not exists
-// so we need create a global variable to store 2 variables
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  //   clear input fields before input them
-  inputDistance.value = inputDuration.value = inputCadence.value = "";
-
-  // display the marker
-  const { lat: currentLatitude, lng: currentLogitude } = mapEvent.latlng;
-  // add the current mouse click address to map
-  L.marker([currentLatitude, currentLogitude])
-    .addTo(map)
-    //   binds a popup to the layer with the passed content
-    .bindPopup(
-      // a new popup instance
-      L.popup({
-        maxWidth: 250,
-        maxHeight: 50,
-        //   set to false if you want to do not close the popup when another popup is opened
-        autoClose: false,
-        //    set to false if you want to do not close the popup when use clicks on the map
-        closeOnClick: false,
-        className: "running-popup",
-      })
-    )
-    .setPopupContent("Workout")
-    .openPopup();
-
-  form.classList.add("hidden");
-});
-
-inputType.addEventListener("change", function () {
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
+const app = new App();
