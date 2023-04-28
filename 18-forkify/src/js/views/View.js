194d3c6ad293e7a90dbd1b85dce3b3cae063278d
VanTruongUIT <tvtruong.uit@gmail.com>
2023-04-28 20:40:43 +0700

    working with forEach and forOf

--- 18-forkify/src/js/views/View.js
+++ 18-forkify/src/js/views/View.js
@@ -2,6 +2,14 @@ import icons from "url:../../img/icons.svg"; // Parcel 2
 
 export default class View {
   _data;
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup strings is returned if render=false
+   * @this {Object} View instance
+   * @author Truong Tran
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0)) {
       return this.renderError();
